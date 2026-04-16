a812e2afdc68a634b8a87c0af2c486380fd649ab
vhuerfano-cmd <v.huerfano@centro.edu.mx>
2026-04-16 13:33:16 -0600

    add changes

--- js/botones.js
+++ js/botones.js
@@ -1,6 +1,23 @@
+let title = document.querySelector("h1");
 let boton1 = document.getElementById("btn-1");
 let boton2 = document.getElementById("btn-2");
 let boton3 = document.getElementById("btn-3");
+boton1.addEventListener("click", function() {
+    title.innerText = "Botón 1 presionado";
+    bgColor("lightblue");
+});
+function mouseEnter() {
+    console.log("Mouse Enter");
+    bgColor("red"); 
+}
+
+function mouseLeave() {
+    bgColor("white");
+}
+
+function bgColor(color) {
+    document.body.style.backgroundColor = color;
+}
 
 boton1.addEventListener("click", function() {
      console.log("Botón 1 presionado");
@@ -9,17 +26,10 @@ boton1.addEventListener("click", function() {
 boton2.addEventListener("click", function() {
      console.log("Botón 2 presionado");
 });
+boton2.addEventListener("mouseenter", mouseEnter);
+boton2.addEventListener("mouseleave", mouseLeave);
 
 boton3.addEventListener("click", function() {
      console.log("Botón 3 presionado");
 });
-function mouseEnerter() {
-    console.log("Mouse Enter");
-}
-function mouseLeave() {
-    console.log("Mouse Leave");
-}
-function bgColor() {
-    console.log("Background Color");
-    
-}
+boton3.addEventListener("mouseenter", mouseEnter);
