58a45ea2a95b8234e22a104441500bf6894b9b4a
vhuerfano-cmd <v.huerfano@centro.edu.mx>
2026-04-21 16:39:55 -0600

    add

---
+++ cursor.js
@@ -0,0 +1,9 @@
+let x = 0; y = 0;
+let stroke_color = "black";
+let stroke_weight = 1;
+let fill_color = "white";
+
+function setup() {
+    createCanvas(windowWidth, windowHeight);
+    background(0);
+}
--- js/botones.js
+++ js/botones.js
@@ -2,10 +2,8 @@ let title = document.querySelector("h1");
 let boton1 = document.getElementById("btn-1");
 let boton2 = document.getElementById("btn-2");
 let boton3 = document.getElementById("btn-3");
-boton1.addEventListener("click", function() {
-    title.innerText = "Botón 1 presionado";
-    bgColor("lightblue");
-});
+let title2 = document.getElementById("title-2");
+
 function mouseEnter() {
     console.log("Mouse Enter");
     bgColor("red"); 
@@ -20,16 +18,22 @@ function bgColor(color) {
 }
 
 boton1.addEventListener("click", function() {
+     title.innerText = "Botón 1 presionado";
      console.log("Botón 1 presionado");
 });
+boton1.addEventListener("mouseenter", mouseEnter);
+boton1.addEventListener("mouseleave", mouseLeave);
 
 boton2.addEventListener("click", function() {
+     title  .innerText = "Botón 2 presionado";
      console.log("Botón 2 presionado");
 });
 boton2.addEventListener("mouseenter", mouseEnter);
 boton2.addEventListener("mouseleave", mouseLeave);
 
 boton3.addEventListener("click", function() {
+     title2.innerText = "Botón 3 presionado";
      console.log("Botón 3 presionado");
 });
 boton3.addEventListener("mouseenter", mouseEnter);
+boton3.addEventListener("mouseleave", mouseLeave);
